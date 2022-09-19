2702ac85520d1db48aaf56c4afe7091834f88a39
Gary Morin <suecmorin@gmial.com>
2022-09-19 07:49:52 -0700

    fixed errors with generate markdown and init functions

--- index.js
+++ index.js
@@ -1,115 +1,78 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const Mkdown = require("./utils/generateMarkdown.js");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
 
 // TODO: Create an array of questions for user input
 
 const questions = [
   {
-    type: 'input',
-    name: 'username',
-    message: 'Please enter your name',
+    type: "input",
+    name: "username",
+    message: "Please enter your name",
   },
   {
-  type: 'input',
-  name: 'github',
-  message: 'Please enter your GitHub account',
-},
-{
-    type: 'input',
-    name: 'projectName',
-    message: 'Project Name:',
+    type: "input",
+    name: "github",
+    message: "Please enter your GitHub account",
   },
   {
-    type: 'input',
-    name: 'description',
-    message: 'Project Description:',
-   
+    type: "input",
+    name: "projectName",
+    message: "Project Name:",
   },
   {
-    type: 'input',
-    name: 'installation',
-    message: 'Installation:',
-    
+    type: "input",
+    name: "description",
+    message: "Project Description:",
   },
   {
-    type: 'input',
-    name: 'usage',
-    message: 'Usage:',
-    
+    type: "input",
+    name: "installation",
+    message: "Installation:",
   },
-{
-type: 'input',
-name: 'contributing',
-message: 'Collaborators:',
-},
-{
-type: 'input',
-name: 'thirdParty',
-message: 'Third party resources or tutorials which were helpful:',
-},
-{
-type: 'checkbox',
-name: 'license',
-choices: 
-[
-{
-name: 'MIT',
-},
-{
-name: 'Apache',
-},
-{
-name: 'GPL',
-},
-{
-name: 'BSD',
-},
-{
-name: 'None',
-}
-],
-}
+  {
+    type: "input",
+    name: "usage",
+    message: "Usage:",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Collaborators:",
+  },
+  {
+    type: "input",
+    name: "thirdParty",
+    message: "Third party resources or tutorials which were helpful:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Which license did you use for this application?",
+    choices: [ 'None', 'MIT', 'Apache', 'GPL', 'BSD'],
+  },
 ];
-function promptUser(){
-  inquirer.prompt(questions).then((answers) => {
-    console.log(JSON.stringify(answers, null, '  '));
-
-   });
-  }
-
 
 // TODO: Create a function to write README file
-function writeToFile(filename, data) {
-  fs.writeFile(fileName, data, function(err){
-    if (err){
-        console.error(err);
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(data);
     }
-});
+  });
 }
 
 // TODO: Create a function to initialize app
-  async function init() {
-    console.log("Welcome to the README Generator!")
-    try {
-        const answers = await promptUser();
-        const readMe = generateMarkdown(data);
-        writeToFile("GeneratedREADME.md", readMe);
-        console.log("**README file successfully created!**");
-        
-    }catch(err) {
-        console.log(err);
-        
-    }
-
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    writeToFile("README.md", generateMarkdown(data))
+    console.log(data)
+  })
+}
 
 // Function call to initialize app
 init();
-    promptUser();
-    renderLicenseBadge();
-    renderLicenseLink();
-    renderLicenseSection();
-    writeToFile();
-  }
-    
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,96 +1,72 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function licenseBadge(license) {
-  if (license === null) {
-    return ``
-  } else if (license = "none") {
-    return ``
-  } else return `![license badge](http://img.shiends.io/badge/license-${license}-blue.svg)`;  
+function renderLicenseBadge(data) {
+  if ((data.license = "None")) {
+    return ``;
+  } else {
+    return `![license badge](http://img.shiends.io/badge/license-${data.license}-blue.svg)`;
+  }
 }
-
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-    switch (license)
-    {
-      case "null":
-      return ``
-      case "none":
-        return ``
-      case "MIT":
-        return `https://mit-license.org/`
-      case "Apache":
-        return `http://www.apache.org/licenses/LICENSE-2.0`
-      case "GPL":
-        return `https://www.gnu.org/licenses/gpl-3.0.en.html`
-      case "BSD":
-        return `https://opensource.org/licenses/alphabetical`
-        ;
-    }
+function renderLicenseLink(data) {
+  let link = data.license;
+  if (link === "MIT") {
+    return `https://mit-license.org/`;
   }
-
+  if (link === "Apache") {
+    return `http://www.apache.org/licenses/LICENSE-2.0`;
+  }
+  if (link === "GPL") {
+    return `https://www.gnu.org/licenses/gpl-3.0.en.html`;
+  }
+  if (link === "BSD") {
+    return `https://opensource.org/licenses/alphabetical`;
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license === null) {
-    return `
-  } else if (license === 'none') {
-    return `
-  } else return `Please refer to license link for exact text of license for your project. `
-  ;
+function renderLicenseSection(data) {
+  if (data.license === null) {
+    return;
+  } else if (data.license === "None") {
+    return "";
+  } else {
+    return `Please refer to license link for exact text of license for your project. `;
+  }
 }
 
-  
 function generateMarkdown(data) {
-
- return `
-  
-  ##Description
-  {data.description}
-
-  ##Table of Contents
-  [Description]  (#description)
-  [Installation] (#installation)
-  [Usage]        (#usage)
-  [License] (#license)
-  [Contributing] (#contributing)
-  [Contact] (#contact)
-
-  ##Description
-  {data.description}
-
-  ##Installation
-  {data.installation}
-  
-
-  ##Usage
-  {data.usage}
-
-  ##License
-  ${renderLicenseBadge(data.license)}
-  ${renderLicenseLink(data.license)}
-  ${renderLicenseSection(data.license)}
-  
-
-
-  
-
-  ##Contributing
-
-  {data.contributing} <br/>
-  {data.thirdParty}
-
-  ##Contact
-  My name is {data.username}. My GitHub account is https://github.com/{github}.
-
-  ` 
-  ;
- }
-
+  return `# ${data.projectName}
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Contact](#contact)
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## License
+  ${renderLicenseBadge(data)}
+  ${renderLicenseLink(data)}
+  ${renderLicenseSection(data)}
+  ## Contributing
+  ${data.contributing} <br/>
+  ${data.thirdParty}
+  ## Contact
+  My name is ${data.username}. My GitHub account is https://github.com/${
+    data.github
+  }.
+  `;
+}
 
 module.exports = generateMarkdown;
-
-
-
